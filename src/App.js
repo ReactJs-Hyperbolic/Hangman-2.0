004bb1c9a9c86b44b3408fbcf6e18e7f1be30635
Karsenh <KarsenHansen@gmail.com>
2021-05-26 01:53:18 -0700

    Added all components and state vars

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React, { useState } from 'react';
 import Header from './Components/Header';
 import Figure from './Components/Figure';
 import WrongLetters from './Components/WrongLetters';
 import Word from './Components/Word';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+// Randomly select a word from the words array to use for a round
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  // Create state for our dynamic variables
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [incorrectLetters, setIncorrect] = useState([]);
+
   return (
     <>
       <Header />
