f4944d2853a08bd5abfbe0b83b764484aef279ad
Karsenh <KarsenHansen@gmail.com>
2021-05-25 23:07:21 -0700

    Initial hangman 2.0 commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <h1>React Hang-Man</h1>
+      <p>Guess the hidden word - Enter a letter to start!</p>
+    </>
+  );
+};
+
+export default Header;
